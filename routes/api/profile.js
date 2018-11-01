b4d490a001875ce0eb86095b947110f417e0974e
Paul Crussaire <paul@crussaire.com>
2018-11-01 12:06:10 +0100

    initial express server with routes

---
+++ routes/api/profile.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/profile/test
+// @desc    Tests profile route
+// @access  Public
+router.get('/test', (req, res, next) => {
+  res.json({msg:'profile OK'})
+})
+
+module.exports = router;
