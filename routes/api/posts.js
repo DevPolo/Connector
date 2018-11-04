c7f6ca90e585ccc4d56912360b9f4c09e1fcca40
Paul Crussaire <paul@crussaire.com>
2018-11-04 16:35:59 +0100

    backend Done

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -10,6 +10,7 @@ const validatePostInput = require('../../validation/post');
 const Post = require('../../models/Post');
 const Profile = require('../../models/Profile');
 
+
 // @route   GET api/posts
 // @desc    Get all posts
 // @access  Public
@@ -23,6 +24,7 @@ router.get('/', (req, res, next) => {
       res.status(404).json(errors);
     });
 });
+
 
 // @route   GET api/posts/:id
 // @desc    Get post by id
@@ -187,6 +189,75 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (re
         res.status(404). json(errors);
       });
   });
+});
+
+
+// @route   POST api/posts/comment/:id
+// @desc    Add a comment
+// @access  Private
+router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+  const { errors, isValid } = validatePostInput(req.body);
+
+  // Check validation
+  if(!isValid) {
+    res.status(400).json(errors);
+  }
+
+  Post.findById(req.params.id)
+    .then(post => {
+      const newComment = {
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id,
+      }
+
+      // Add to comments array
+      post.comments.unshift(newComment);
+
+      // Save
+      post.save()
+        .then(post => {
+          res.json(post);
+        });
+    })
+    .catch(err => {
+      errors.postnotfound = 'No post found';
+      res.status(404).json(errors);
+    });
+});
+
+
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Delete a comment from post
+// @access  Private
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+  const errors = {};
+
+  Post.findById(req.params.id)
+    .then(post => {
+
+      // Check if the comment exists
+      if(post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0) {
+        errors.commentnotexists = 'Comment does not exists';
+        res.status(404).json(errors);
+      }
+
+      // Get remove index
+      const removeIndex = post.comments
+      .map(item => item._id.toString())
+      .indexOf(req.params.comment_id);
+
+      // Splice comment out of array
+      post.comments.splice(removeIndex, 1);
+
+      // Save
+      post.save().then(post => res.json(post));
+    })
+    .catch(err => {
+      errors.postnotfound = 'No post found';
+      res.status(404).json(errors);
+    });
 });
 
 module.exports = router;
